d1df0889d3ec777d1656f258fe9e70c0b6206a2a
Ethan Bork <ebork11@gmail.com>
2024-06-29 10:04:13 -0700

    Initial Commit

---
+++ src/client/java/borknbeans/artisanstools/ArtisansToolsClient.java
@@ -0,0 +1,10 @@
+package borknbeans.artisanstools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArtisansToolsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/borknbeans/artisanstools/ArtisansToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package borknbeans.artisanstools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ArtisansToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
