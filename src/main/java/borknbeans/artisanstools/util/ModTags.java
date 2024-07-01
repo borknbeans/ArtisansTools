c6c9b3f2e3383b269db65fce2c89cfdd93404129
Ethan Bork <ebork11@gmail.com>
2024-07-01 11:41:48 -0700

    Tags are working. Thank you KaupenJoe

---
+++ src/main/java/borknbeans/artisanstools/util/ModTags.java
@@ -0,0 +1,30 @@
+package borknbeans.artisanstools.util;
+
+import borknbeans.artisanstools.ArtisansTools;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+public class ModTags {
+    public static class Blocks {
+
+        private static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(ArtisansTools.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+        public static final TagKey<Item> TOOL_PARTS = createTag("tool_parts");
+
+        public static final TagKey<Item> PICKAXE_HEADS = createTag("pickaxe_heads");
+        public static final TagKey<Item> HANDLES = createTag("handles");
+        public static final TagKey<Item> BINDINGS = createTag("bindings");
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(ArtisansTools.MOD_ID, name));
+        }
+    }
+}
